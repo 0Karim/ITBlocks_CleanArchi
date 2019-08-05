fdc3e4658ed0161539f57bd0d33cba6ab42efc6c
Karim <karimalaa298@gmail.com>
2019-08-05 13:46:13 +0200

    Add Course Domain Model

---
+++ ASP/CleanArch.Domain/Models/Course.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanArch.Domain.Models
+{
+    public class Course
+    {
+        public int Id { set; get; }
+        public string Name { set; get; }
+        public string Description { set; get; }
+        public string ImageUrl { set; get; }
+    }
+}
