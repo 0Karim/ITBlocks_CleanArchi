2085c49dae88c5f6fcc6b4a13c0ac7e36ff12052
Karim <karimalaa298@gmail.com>
2019-08-07 00:33:06 +0200

    Configure Presentation Layer for Dependency Container

--- ASP/CleanArch.Mvc/Startup.cs
+++ ASP/CleanArch.Mvc/Startup.cs
@@ -14,6 +14,7 @@ using CleanArch.Mvc.Data;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using CleanArch.Infra.Data.Context;
+using CleanArch.Infra.IoC;
 
 namespace CleanArch.Mvc
 {
@@ -48,6 +49,9 @@ namespace CleanArch.Mvc
                 options.UseSqlServer(Configuration.GetConnectionString("UnivirsityDBConnection")));
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+
+            RegisterServices(services);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -77,6 +81,16 @@ namespace CleanArch.Mvc
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
             });
+        }
+
+
+        /**
+         * this method allow us to hook up dependency Container
+         * Use this method to configure the dependency Injection.
+         */
+        public static void RegisterServices(IServiceCollection services)
+        {
+            DependencyContainer.RegisterServices(services);
         }
     }
 }
