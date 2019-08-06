fe52fb97c2e06de620daac18f295e3e1d9d9a57e
Karim <karimalaa298@gmail.com>
2019-08-06 09:39:57 +0200

    Update New CleanArch.Mvc Presentation Layer

--- ASP/CleanArch.Mvc/Data/Migrations/20190806072642_IntialMigration.Designer.cs
+++ ASP/CleanArch.Mvc/Data/Migrations/20190806072642_IntialMigration.Designer.cs
@@ -10,14 +10,14 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CleanArch.Mvc.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    [Migration("20190805095126_IntialMigration")]
+    [Migration("20190806072642_IntialMigration")]
     partial class IntialMigration
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("ProductVersion", "2.1.11-servicing-32099")
+                .HasAnnotation("ProductVersion", "2.2.6-servicing-10079")
                 .HasAnnotation("Relational:MaxIdentifierLength", 128)
                 .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
 
