b6838b4e4dc0f8473c261c962b2d691701a6ff1e
Karim <karimalaa298@gmail.com>
2019-08-07 11:34:17 +0200

    Adding DomainCore and Implementation of InMemoryBus

--- ASP/CleanArch.Infra.Bus/InMemoryBus.cs
+++ ASP/CleanArch.Infra.Bus/InMemoryBus.cs
@@ -1,11 +1,30 @@
-﻿using System;
+﻿using CleanArch.Domain.Core.Bus;
+using CleanArch.Domain.Core.Commands;
+using MediatR;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace CleanArch.Infra.Bus
 {
-    public class InMemoryBus
+    public sealed class InMemoryBus : IMediatorHandler
     {
+        private readonly IMediator _mediator;
 
+        public InMemoryBus(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        /*
+         * send the command throught the bus
+         * create a round wrapper 
+         * that send the command of type T is take argument of type T from our Domain Layer
+         */
+        public Task SendCommand<T>(T command) where T : Command
+        {
+            return _mediator.Send(command);
+        }
     }
 }
